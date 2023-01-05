80de6d2155dd668c78d5f4020383d50110435b94
WillDrac <willdrac@yahoo.com>
2023-01-05 15:10:57 -0500

    initial scan writing to file

--- replayMod.lua
+++ replayMod.lua
@@ -18,11 +18,14 @@ function startRecording()
     centerY = math.floor(py)
     centerZ = math.floor(pz)
 
-    fs.delete("initialTEMP.replay")
-    initialScanFile = fs.open("initialTEMP.replay", "w")
+    fs.delete(fileName.value .. ".replay")
+    saveFile = fs.open(fileName.value .. ".replay", 'w')
+
+    saveFile:seek(5)
 end
 
-initialScanFile = nil
+updatesFile = nil
+saveFile = nil
 
 function endRecording()
     recording = false
@@ -30,7 +33,13 @@ function endRecording()
     endRecordingButton.visible = false
     status = "Waiting to record."
 
-    initialScanFile:close()
+    saveFile:seek(0)
+    -- packet Id
+    saveFile:writeByte(1)
+    -- Number of  blocks to update
+    saveFile:writeUInt(blocksSaved)
+
+    saveFile:close()
 end
 
 client.settings.addTitle("status")
@@ -40,29 +49,43 @@ endRecordingButton.visible = false
 fileName = client.settings.addNamelessTextbox("Save file name:", "Untitled")
 
 function test()
-    local file = fs.open("initialTEMP.replay", 'r')
-    for i = 1, 5, 1 do
-        print(file:readInt())
-        print(file:readInt())
-        print(file:readInt())
-        print(file:readUInt())
-        print(file:readByte())
+    -- making a file of the initial scan that is readable
+    local input = fs.open(fileName.value .. ".replay", 'r')
+    local output = io.open("text.txt", "w+")
+    if input and output then
+        output:write(input:readByte())
+        local numBlocks = input:readUInt()
+        output:write("\n")
+        output:write(numBlocks)
+        output:write("\n")
+        for i = 1, numBlocks, 1 do
+            output:write(input:readInt())
+            output:write(" ")
+            output:write(input:readInt())
+            output:write(" ")
+            output:write(input:readInt())
+            output:write(" ")
+            output:write(input:readUShort())
+            output:write(" ")
+            output:write(input:readByte())
+            output:write("\n")
+        end
+        output:close()
     end
-    file:close()
 end
 
 client.settings.addFunction("Test Button", "test", "Enter")
 
 ------------------------------------------------------------
 
-initialBlocksScannedPerUpdate = 10
+initialBlocksScannedPerUpdate = 10000
 function update()
     px, py, pz = player.pposition()
     pYaw, pPitch = player.rotation()
 
     -- initial scan
     if recording then
-        print(initalBlocksScanned)
+        print(blocksSaved)
         coroutine.resume(initialScan)
     end
 
@@ -76,16 +99,39 @@ end
 -- adds a block to the world scan table
 function addToWorldScan(x, y, z)
     local block = dimension.getBlock(math.floor(x), math.floor(y), math.floor(z))
+    initalBlocksScanned = initalBlocksScanned + 1
 
-    initialScanFile:writeInt(x)
-    initialScanFile:writeInt(y)
-    initialScanFile:writeInt(z)
-    initialScanFile:writeUInt(block.id)
-    initialScanFile:writeByte(block.data)
+    if block.id ~= 0 then
+        blocksSaved = blocksSaved + 1
+
+        saveFile:writeInt(x)
+        saveFile:writeInt(y)
+        saveFile:writeInt(z)
+        saveFile:writeUShort(block.id)
+        saveFile:writeByte(block.data)
+    end
 end
 
--- a coroutine loop that puts blocks from the world into a table
-initalBlocksScanned = 0
+-- y = max(centerY - math.floor((r - 1) / 2), -worldBorder), min(centerY + math.floor((r - 1) / 2), worldBorder)
+
+-- get the world height border
+_versionSplit = string.split(client.mcversion, ".")
+_v = {}
+for i = 1, #_versionSplit, 1 do
+    table.insert(_v, tonumber(_versionSplit[i]))
+end
+worldMaxHeight = 320
+worldMinHeight = -64
+if _v[1] <= 1 and _v[2] < 18 then
+    -- is an old world
+    worldMaxHeight = 256
+    worldMinHeight = 0
+end
+--
+
+initalBlocksScanned = 0 -- counts the amount of blocks scanned to be used to guide the coroutine
+blocksSaved = 0 -- this is different from the one above because it does not count air blocks, which do not get saved to the file
+-- a coroutine loop that puts blocks from the world into a file
 initialScan = coroutine.create(function(...)
     for r = 1, 10000 do
         -- Z-axis
@@ -93,8 +139,6 @@ initialScan = coroutine.create(function(...)
             for y = centerY - math.floor((r - 1) / 2), centerY + math.floor((r - 1) / 2) do
                 for i = -1, 1, 2 do
                     addToWorldScan(x, y, centerZ + (r - 1) * i)
-
-                    initalBlocksScanned = initalBlocksScanned + 1
                     if initalBlocksScanned % initialBlocksScannedPerUpdate == 0 then
                         coroutine.yield()
                     end
@@ -107,8 +151,6 @@ initialScan = coroutine.create(function(...)
             for y = centerY - math.floor((r - 1) / 2), centerY + math.floor((r - 1) / 2) do
                 for i = -1, 1, 2 do
                     addToWorldScan(centerX + (r - 1) * i, y, z)
-
-                    initalBlocksScanned = initalBlocksScanned + 1
                     if initalBlocksScanned % initialBlocksScannedPerUpdate == 0 then
                         coroutine.yield()
                     end
@@ -117,13 +159,13 @@ initialScan = coroutine.create(function(...)
         end
 
         -- Y-axis
+
+        -- split into one for up, one for down
         if r % 2 == 0 then
             for x = centerX - (r - 3), centerX + (r - 3) do
                 for z = centerZ - (r - 3), centerZ + (r - 3) do
                     for i = -1, 1, 2 do
                         addToWorldScan(x, centerY + math.floor((r - 1) / 2) * i, z)
-
-                        initalBlocksScanned = initalBlocksScanned + 1
                         if initalBlocksScanned % initialBlocksScannedPerUpdate == 0 then
                             coroutine.yield()
                         end
@@ -173,6 +215,10 @@ end)
 ]]
 
 --[[
+
+    TODO: make the y scan capped to the world height limits
+    TODO: make a seperate set of player xyz that are Ints, instead of rounding later?
+
     save the block update data for the initial load in one temp file
     save all the updates into another temp file
     then, when the save button is pressed, a new file is created (this will be the file that the user keeps to play back) it includes (in order)
@@ -184,7 +230,6 @@ end)
     add a way to stop the world scan coroutine so that you can record two times in a row (something with errors?)
         now, stopping recording and restarting it will just continue the same loop
 
-    x and z grow twice as fast as y because worlds are usually more flat than tall
     scan start: starts the initial world scan starting at the player's inital position, then starts the smaller scan following the player
     scan end: writes everything that has been stored into a file, formatted
     player position can be interpolated
