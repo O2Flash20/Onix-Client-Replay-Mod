e0cbf571f054d36c6dd5ad1058c1c9fad8f1b7f7
WillDrac <willdrac@yahoo.com>
2023-01-18 18:53:04 -0500

    ui, recording and playing in pre-alpha stage

--- replayMod.lua
+++ replayMod.lua
@@ -5,6 +5,8 @@ workingDir = "RoamingState/OnixClient/Scripts/Data/ReplayMod"
 
 importLib("logger")
 
+importLib("keyconverter")
+
 ---@type BinaryFile|nil
 updatesFile = nil
 ---@type BinaryFile|nil
@@ -14,12 +16,14 @@ saveFile = nil
 status = "Waiting to record."
 recording = false
 blockList = {}
+timeRecordingStarted = nil
 function startRecording()
     blockList = {}
     recording = true
     startRecordingButton.visible = false
     endRecordingButton.visible = true
     status = "Recording..."
+    timeRecordingStarted = os.clock()
 
     centerX = pxInt
     centerY = pyInt
@@ -44,7 +48,13 @@ function endRecording()
     endRecordingButton.visible = false
     status = "Waiting to record."
 
-    -- * MERGE THE TEMP FILE IN RIGHT HERE
+    -- merge updates temp file into the save file
+    updatesFile:close()
+    updatesFile = fs.open("updates.TEMP", "r")
+    print("Size: " .. updatesFile:size())
+    saveFile:writeRaw(updatesFile:readRaw(updatesFile:size()))
+    updatesFile:close()
+    fs.delete("updates.TEMP")
 
     -- Save hash->name and data
     saveFile:writeUByte(0)
@@ -61,7 +71,6 @@ function endRecording()
     saveFile:writeUShort(arrayLength)
     print("Size of block list: " .. arraySize .. " length: " .. arrayLength)
 
-
     -- start off the packet of the initial block update
     saveFile:seek(0)
     -- packet Id
@@ -70,7 +79,6 @@ function endRecording()
     saveFile:writeUInt(blocksSaved)
 
     saveFile:close()
-    updatesFile:close()
 end
 
 endRecordingButton = client.settings.addFunction("End Recording", "endRecording", "Save")
@@ -84,12 +92,113 @@ fileName = client.settings.addNamelessTextbox("Save file name:", "Untitled")
 -- making a file that is readable for degugging
 function test()
     --reading the updates file
-    local input = fs.open("updates.TEMP", "r")
-    local output = io.open("text.txt", "w+")
+    --[[
+        local input = fs.open("updates.TEMP", "r")
+        local output = io.open("text.txt", "w+")
+    
+        if input == nil or output == nil then return end
+    
+        -- loop through all updates
+        while input:eof() == false do
+            output:write("\n") --new line
+            local id = input:readUByte() --get the packet id
+    
+            if id == 255 then
+                output:write("____update____")
+            elseif id == 1 then -- a block update exists
+                output:write("BLOCK: ")
+                local numberOfBlocks = input:readUInt()
+                output:write("(" .. numberOfBlocks .. ") ")
+    
+                if numberOfBlocks < 10 then
+                    for i = 1, numberOfBlocks do
+                        output:write(input:readInt() .. " ")
+                        output:write(input:readInt() .. " ")
+                        output:write(input:readInt() .. " ")
+                        output:write(input:readLong() .. " ")
+                    end
+                else
+                    for i = 1, numberOfBlocks do
+                        output:write("Loop")
+                        input:readInt()
+                        input:readInt()
+                        input:readInt()
+                        input:readLong()
+                    end
+                end
+            elseif id == 2 then -- this is a player update
+                output:write("PLAYER: ")
+                output:write(input:readFloat() .. " ")
+                output:write(input:readFloat() .. " ")
+                output:write(input:readFloat() .. " ")
+                output:write(input:readByte() .. " ")
+                output:write(input:readByte() .. " ")
+            else
+                output:write("Unknown id " .. id)
+            end
+        end
+    
+        input:close()
+        output:close()
+    ]]
 
+    --reading the initial scan file
+    --[[
+        local input = fs.open(fileName.value .. ".replay", 'r')
+        local output = io.open("text.txt", "w+")
+        if input and output then
+            output:write(input:readUByte())
+            local numBlocks = input:readUInt()
+            output:write("\n")
+            output:write(numBlocks)
+            output:write("\n")
+            for i = 1, numBlocks, 1 do
+                output:write(input:readInt())
+                output:write(" ")
+                output:write(input:readInt())
+                output:write(" ")
+                output:write(input:readInt())
+                output:write(" ")
+                output:write(input:readUShort())
+                output:write(" ")
+                output:write(input:readByte())
+                output:write("\n")
+            end
+            output:close()
+        end
+    ]]
+
+    --reading the combined file
+    local input = fs.open(fileName.value .. ".replay", 'r')
+    local output = io.open("text.txt", "w+")
     if input == nil or output == nil then return end
 
-    -- loop through all updates
+    -- hash information
+    print(input:tell())
+    local blockRegistry = readBlockRegistry(input)
+    print("Length of Block Registry: " .. #blockRegistry)
+    input:seek(0)
+    print(input:size() .. " " .. input:tell())
+    for hash, block in pairs(blockRegistry) do
+        output:write(hash .. " " .. block.name .. " " .. block.data .. "\n")
+    end
+
+    -- initial scan
+    local scanPacketNumber = input:readUByte()
+    local numBlocks = input:readUInt()
+    output:write("\n" .. numBlocks .. " initial scan blocks found: \n")
+    for i = 1, numBlocks do
+        output:write(input:readInt())
+        output:write(" ")
+        output:write(input:readInt())
+        output:write(" ")
+        output:write(input:readInt())
+        output:write(" ")
+        output:write(input:readLong())
+        output:write("\n")
+    end
+
+    -- updates
     while input:eof() == false do
         output:write("\n") --new line
         local id = input:readUByte() --get the packet id
@@ -124,6 +233,9 @@ function test()
             output:write(input:readFloat() .. " ")
             output:write(input:readByte() .. " ")
             output:write(input:readByte() .. " ")
+        elseif id == 0 then -- block hash information
+            output:write("\n HIT HASH INFORMATION - STOPPING")
+            break
         else
             output:write("Unknown id " .. id)
         end
@@ -131,32 +243,6 @@ function test()
 
     input:close()
     output:close()
-
-    --reading the initial scan file
-    --[[
-        local input = fs.open(fileName.value .. ".replay", 'r')
-        local output = io.open("text.txt", "w+")
-        if input and output then
-            output:write(input:readUByte())
-            local numBlocks = input:readUInt()
-            output:write("\n")
-            output:write(numBlocks)
-            output:write("\n")
-            for i = 1, numBlocks, 1 do
-                output:write(input:readInt())
-                output:write(" ")
-                output:write(input:readInt())
-                output:write(" ")
-                output:write(input:readInt())
-                output:write(" ")
-                output:write(input:readUShort())
-                output:write(" ")
-                output:write(input:readByte())
-                output:write("\n")
-            end
-            output:close()
-        end
-    ]]
 end
 
 client.settings.addFunction("Convert .replay to .txt", "test", "Enter")
@@ -170,7 +256,7 @@ function readSingleBlockData(file)
     output.x = file:readInt()
     output.y = file:readInt()
     output.z = file:readInt()
-    output.hash = file:readLong() --block hash -> to get name and data from registry
+    output.hash = file:readLong() --block hash to get name and data from registry
 
     return output
 end
@@ -198,45 +284,101 @@ function readBlockRegistry(file)
 end
 
 -- reads the file and sets blocks in the world
-function worldFromFile()
-    local file = fs.open(fileName.value .. ".replay", "r")
-    if file == nil then return nil end
+loadingFile = nil
+function startWorldLoad()
+    -- allow updates to start at the origin
+    centerX = pxInt
+    centerY = pyInt
+    centerZ = pzInt
+
+    loadingFile = fs.open(fileName.value .. ".replay", "r")
+    if loadingFile == nil then return nil end
 
     -- Get block list
-    local blockRegistry = readBlockRegistry(file)
+    blockRegistry = readBlockRegistry(loadingFile)
 
-    print("Packet ID (should be 1): " .. file:readByte())
-    local amountOfBlocks = file:readUInt()
+    print("Packet ID (should be 1): " .. loadingFile:readByte())
+    local amountOfBlocks = loadingFile:readUInt()
     print("Amount of blocks: " .. amountOfBlocks)
 
     for i = 1, amountOfBlocks do
-        data = readSingleBlockData(file)
-        -- print(blockRegistry[data[4]].name)
-        client.execute(
-            "execute /setblock ~" ..
-            data.x ..
-            " ~" ..
-            data.y ..
-            " ~" ..
-            data.z ..
-            " " ..
-            blockRegistry[data.hash].name ..
-            " " ..
-            blockRegistry[data.hash].data
-        )
+        data = readSingleBlockData(loadingFile)
+        placeBlock(data.x, data.y, data.z, blockRegistry[data.hash].name, blockRegistry[data.hash].data, true)
     end
 
-    file:close()
+    -- starts off the coroutine every update
+    -- loading = true
+    -- *DONE USING THE NUMBER 8 INSTEAD
 end
 
-loading = false
-client.settings.addFunction("Load World", "worldFromFile", "Enter")
-registerCommand("loading", function() loading = true end) -- I'm on touchpad atm so this is less painful
+replayLoad = coroutine.create(function()
+    -- could be while true tbh
+    while loadingFile:eof() == false do
+        local packetId = loadingFile:readUByte()
 
+        if packetId == 255 then
+            -- new update
+            coroutine.yield()
+
+        elseif packetId == 1 then
+            print("hit a block packet")
+            -- block change
+            local amountOfBlocks = loadingFile:readUInt()
+            for i = 1, amountOfBlocks do
+                print(">")
+                local data = readSingleBlockData(loadingFile)
+                placeBlock(data.x + centerX, data.y + centerY, data.z + centerZ, blockRegistry[data.hash].name,
+                    blockRegistry[data.hash].data)
+            end
+
+        elseif packetId == 2 then
+            -- player pos and rot
+            local x = loadingFile:readFloat()
+            local y = loadingFile:readFloat()
+            local z = loadingFile:readFloat()
+            local yaw = loadingFile:readByte()
+            local pitch = loadingFile:readByte()
+            client.execute(
+                "execute /tp @s "
+                .. x + centerX .. " "
+                .. y + centerY - 1.6 .. " "
+                .. z + centerZ .. " "
+                .. yaw * 1.44 .. " "
+                .. pitch
+            )
+
+        elseif packetId == 0 then
+            break
+        end
+        print("loop")
+    end
+    print('REPLAY ENDED')
+    loadingFile:close()
+    loading = false
+end)
+
+function xyzFromYawPitch(playerX, playerY, playerZ, playerPitch, playerYaw, dist)
+    playerPitch = math.rad(playerPitch)
+    playerYaw = math.rad(-playerYaw)
+
+    y = -dist * math.sin(playerPitch)
+    z = dist * math.cos(playerPitch)
+    x = z * math.sin(playerYaw)
+    z = z * math.cos(playerYaw)
+
+    return { x + playerX, y + playerY, z + playerZ }
+end
+
+client.settings.addFunction("Load World", "startWorldLoad", "Enter")
+-- registerCommand("loading", function() loading = true end)
+
+loading = false
 scanShouldStop = false
-lastWasRecoding = false --Keeps track of if the last update frame was recording or not. If it used to be recording but now isn't, the initial scan coroutine will be called one last time, but it will be reset to the start for next time.
-initialBlocksScannedPerUpdate = 1000
+lastWasRecoding = false --Keeps track of if the last update frame was recording or not. If it used to be recording but now isn't, the initial scan coroutine will be called one last time, so that it will be reset to the start for next time.
+initialBlocksScannedPerUpdate = 5000
 function update()
+    log(loading)
+
     pxInt, pyInt, pzInt = player.position()
     px, py, pz = player.pposition()
     pYaw, pPitch = player.rotation()
@@ -272,20 +414,17 @@ function update()
 
         -- write player position and rotation
         updatesFile:writeUByte(2)
-        updatesFile:writeFloat(px)
-        updatesFile:writeFloat(py)
-        updatesFile:writeFloat(pz)
+        updatesFile:writeFloat(px - centerX)
+        updatesFile:writeFloat(py - centerY)
+        updatesFile:writeFloat(pz - centerZ)
         updatesFile:writeByte(math.floor(pYaw / 1.44))
         updatesFile:writeByte(math.floor(pPitch))
     end
 
     if loading then
-        loading = false
-        worldFromFile()
-
-        -- coroutine.resume(replayLoad) or something
-
+        coroutine.resume(replayLoad)
     end
+
     blockChangesThisUpdate = {}
 
     lastWasRecoding = recording
@@ -294,10 +433,11 @@ end
 blockChangesThisUpdate = {}
 -- detecting block changes
 event.listen("BlockChanged", function(x, y, z, newBlock, oldBlock)
-    if blockList[newBlock] == nil then
+    if recording and blockList[newBlock.hash] == nil then
         --  get block id and name, add to blocklist
         local block = dimension.getBlock(x, y, z)
-        blockList[newBlock] = { name = block.name, data = block.data }
+        print("Inserting new hash (from new block in update)")
+        blockList[newBlock.hash] = { name = block.name, data = block.data }
     end
     table.insert(blockChangesThisUpdate, { x = x, y = y, z = z, hash = newBlock.hash })
 end)
@@ -431,9 +571,84 @@ initialScan = coroutine.create(function()
     end
 end)
 
+-- UI ELEMENTS
+positionX = 50
+positionY = 50
+sizeX = 100
+sizeY = 50
+function render2()
+    gfx2.color(38, 38, 38, 230)
+    gfx2.fillRoundRect(0, 0, 100, 50, 2)
+    gfx2.color(255, 255, 255, 115)
+    gfx2.drawRoundRect(0, 0, 100, 50, 1, 1)
+
+    -- title
+    gfx2.color(255, 255, 255)
+    gfx2.text(33, 2, "Replay Mod", 1)
+
+    -- status
+    if recording then
+        gfx2.text(2, 13, "Status:       [" .. stylizedClock(timeRecordingStarted, os.clock()) .. "]", 0.8)
+        gfx2.color(230, 20, 0)
+        gfx2.fillElipse(23, 17, 2.5)
+        gfx2.color(255, 255, 255)
+    else
+        gfx2.text(2, 13, "Status:", 0.8)
+        gfx2.color(20, 20, 20)
+        gfx2.fillElipse(23, 17, 2.5)
+        gfx2.color(255, 255, 255)
+    end
+
+    -- file
+    gfx2.text(2, 21, "Selected File: " .. fileName.value .. ".replay", 0.8)
+
+    -- toggle button
+    if recording then
+        gfx2.text(2, 29, "Press [" .. keytostr(toggleKey) .. "] to Stop Recording", 0.8)
+    else
+        gfx2.text(2, 29, "Press [" .. keytostr(toggleKey) .. "] to Start Recording", 0.8)
+    end
+end
+
+toggleKey = 0x48 -- (H)
+client.settings.addKeybind("Start/Stop Key", "toggleKey")
+
+event.listen("KeyboardInput", function(key, down)
+    if key == toggleKey and down then
+        -- toggle key was pressed
+        if recording == false then startRecording()
+        else endRecording() end
+
+        return true
+    end
+    if key == 0x38 then
+        loading = true
+    end
+end)
+
+function stylizedClock(start, now)
+    seconds = math.floor(now - start)
+    minutes = math.floor(seconds / 60)
+    seconds = seconds % 60
+
+    if seconds < 10 then seconds = "0" .. seconds end
+    if minutes < 10 then minutes = "0" .. minutes end
+
+    return minutes .. ":" .. seconds
+end
+
+-- places a block in the world
+function placeBlock(x, y, z, name, data, relative)
+    if relative then
+        client.execute("execute /setblock ~" .. x .. " ~" .. y .. " ~" .. z .. " " .. name .. " " .. data)
+    else
+        client.execute("execute /setblock " .. x .. " " .. y .. " " .. z .. " " .. name .. " " .. data)
+    end
+end
+
 --[[
-    put the updates and inital file into one (shouldn't be too hard)
-    add inventory stuff to updates
-    do a demo of replaying (load coroutine)
-    add a graphic telling you if you're recording and for how long
+    ! pressing 8 only works the first time for some reason (i gotta fix that)
+    *use interpolation in render() to make playback smooth
+    *save inventory information
+    *save flags and figure out how to use them to do animations
 ]]
